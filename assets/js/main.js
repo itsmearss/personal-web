f98c0263df2a5888bff2a3675cd7fa466da04255
itsmearss <annurriyadhus17@gmail.com>
2024-05-07 02:28:37 +0700

    personalweb

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,135 @@
 /*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-/*===== CHANGE BACKGROUND HEADER =====*/ 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
-/*===== SHOW SCROLL TOP =====*/ 
+/*===== CHANGE BACKGROUND HEADER =====*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 200) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
+/*===== SHOW SCROLL TOP =====*/
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
 
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+/*===== SHOW SCROLL TOP =====*/
 
-/* Link active portfolio */ 
+/*===== MIXITUP FILTER PORTFOLIO =====*/
+const mixer = mixitup(".portfolio__container", {
+  selectors: {
+    target: ".portfolio__content",
+  },
+  animation: {
+    duration: 400,
+  },
+});
 
+/* Link active portfolio */
+const linkPortfolio = document.querySelectorAll(".portfolio__item");
 
-/*===== SWIPER CAROUSEL =====*/ 
+function activePortfolio() {
+  if (linkPortfolio) {
+    linkPortfolio.forEach((L) => L.classList.remove("active-portfolio"));
+    this.classList.add("active-portfolio");
+  }
+}
+linkPortfolio.forEach((L) => L.addEventListener("click", activePortfolio));
 
+/*===== SWIPER CAROUSEL =====*/
+const mySwiper = new Swiper(".testimonial__container", {
+  // Optional parameters
+  spaceBetween: 16,
+  loop: true,
+  grabCursor: true,
 
-/*===== GSAP ANIMATION =====*/ 
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-
+/*===== GSAP ANIMATION =====*/
+gsap.from(".home__img", { opacity: 0, duration: 2, delay: 0.5, x: 60 });
+gsap.from(".home__data", { opacity: 0, duration: 2, delay: 0.8, x: 25 });
+gsap.from(".home__greeting, .home__name, .home__profession, .home__button", {
+  opacity: 0,
+  duration: 2,
+  delay: 1,
+  x: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.5,
+  x: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.5,
+  x: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+gsap.from(".home__social-icon", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.8,
+  x: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
